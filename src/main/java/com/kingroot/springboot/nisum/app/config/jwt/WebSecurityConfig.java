978e8dc7e7e25e8cc304940b018917d5a46229a6
Sandro Pineda <sandroes1018@gmail.com>
2023-03-01 00:05:48 -0500

    Update WebSecurityConfig.java
    Se comenta funcionalidad de JWT por error a la hora de generarse

--- src/main/java/com/kingroot/springboot/nisum/app/config/jwt/WebSecurityConfig.java
+++ src/main/java/com/kingroot/springboot/nisum/app/config/jwt/WebSecurityConfig.java
@@ -4,11 +4,9 @@ package com.kingroot.springboot.nisum.app.config.jwt;
 
 //@Configuration
 public class WebSecurityConfig  {
-/*
+	/*
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
-		
-		
 		
 		return http
 				.csrf().disable()
@@ -24,8 +22,7 @@ public class WebSecurityConfig  {
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
 				.build();
-	}
-	
+	}	
 	@Bean
 	UserDetailsService userDatailService() {
 		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
@@ -34,16 +31,14 @@ public class WebSecurityConfig  {
 				.roles()
 				.build());
 		return manager;
-	}
-	
+	}	
 	@Bean
 	AuthenticationManager autManager(HttpSecurity http) throws Exception {
 		return http.getSharedObject(AuthenticationManagerBuilder.class)
 				.userDetailsService(userDatailService())
 				.passwordEncoder(passwordEncoder())
 				.and().build();
-	}
-	
+	}	
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
